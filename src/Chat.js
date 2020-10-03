925ee21bdfc490d41295d4e64f8835e7f85e283a
khetwal-om <khetwalom@gmail.com>
2020-10-03 07:24:02 +0530

    3 Dynamic Chat rooms :)

--- src/Chat.js
+++ src/Chat.js
@@ -1,17 +1,31 @@
+
+import React,{useEffect,useState} from 'react'
+
 import { InfoOutlined, StarBorderOutlined } from '@material-ui/icons'
-import React from 'react'
 import {useParams} from 'react-router-dom'
  
 import './Chat.css'
+import db from './firebase'
 
  
 function Chat() {
+    const {roomId}=useParams()
+    const [roomDetails,setRoomDetails]=useState(null)
+      
+    
+    useEffect(()=>{
+          if(roomId){ 
+              db.collection('collection').doc(roomId).onSnapshot(snapshot=> (   
+                    setRoomDetails(snapshot.data())
+              ))
+          }
+    },[roomId])
     return (
         <div className="chat">
             <div className="chat__header">
                  <div className="chat__headerLeft">
                    <h4 className="chat__channelName">                           
-                    <strong>#  general </strong>
+                    <strong>#  {roomDetails ? roomDetails.name : ''} </strong>  
                     <StarBorderOutlined />
                    
                    </h4> 
@@ -28,3 +42,16 @@ function Chat() {
 }
 
 export default Chat
+
+
+
+
+
+
+
+
+
+
+
+
+// roomDetails?.name  
